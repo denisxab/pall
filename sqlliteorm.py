09b51df7bc59dc5ca868ff218e644a57e093805b
denis-kustov@rambler.ru <denisKustov2000kustovDenis>
2021-06-23 21:01:59 +0300

    работает

--- sqlliteorm.py
+++ sqlliteorm.py
@@ -12,6 +12,81 @@ class SqlName:
     IDAUTO: str = "PRIMARY KEY AUTOINCREMENT"  # Авто заполение строки. подходит для id
 
 
+def toDictSql(sql_request: str) -> dict:
+    """
+    :param sql_request: Sql запрос
+    :return: словарь который поддерживает класс SqlLite
+    """
+    res_dict: dict = {}
+    # Конвертировать SQL запросв в header_db
+    for d in sql_request[1:-1:].split(","):
+
+        k, v = d.strip().split(" ")
+        if v in ["TEXT", "text"]:
+            v = str
+        elif v in ["INTEGER", "integer"]:
+            v = int
+        elif v in ["REAL", "real"]:
+            v = float
+        elif v in ["BLOB", "blob"]:
+            v = bytes
+        elif v in ["NULL", "null"]:
+            v = None
+        else:
+            raise TypeError(
+                "Указан не верный тип данных выбирете TEXT;INTEGER;REAL;BLOB;NULL\n{0}:{1}".format(k, v))
+
+        res_dict[k] = v
+
+    return res_dict
+
+
+def toSqlRequest(data: dict) -> str:
+    """
+    :param data: словарь по стандартам класса SqlLite
+    :return: SQl запрос
+    """
+    count_primary_key = False
+    # Конвертация словаря в SQL запрос
+    res = "("
+    for k, v in data.items():
+        res += str(k)
+
+        if type(v) == tuple and len(v) == 2:  # Комбенированная запись с параметрами столбца
+
+            if v[1] == SqlName.PK or v[1] == SqlName.IDAUTO:  # Проверка уникальности Primary Key в таблицы
+                if not count_primary_key:
+                    count_primary_key = True
+                else:
+                    raise LookupError("Запрос имеет больше одного primary key")
+
+            tmp = " {0}".format(v[1])
+            values = v[0]
+        else:  # Обыная запись столбца
+            tmp = ''
+            values = v
+
+        if values == str:
+            res += " TEXT"
+        elif values == int:
+            res += " INTEGER"
+        elif values == float:
+            res += " REAL"
+        elif values == bytes:
+            res += " BLOB"
+        elif values is None:
+            res += " NULL"
+        else:
+            raise TypeError(
+                "Указан не верный тип данных выбирете str;int;float;None;bytes\n{0}:{1}".format(k, v))
+
+        res += "{0}{1}".format(tmp, ", ")
+
+    res = res[:-2:]
+    res += ");"
+    return res
+
+
 class SqlLite:
     """
     - Запись данных в БД
@@ -25,14 +100,14 @@ class SqlLite:
 
     """
 
-    def __init__(self, name_db: str) -> None:  # +
+    def __init__(self, name_dbf: str) -> None:  # +
         self.connection = None
         # Проверка того что разшерение db
-        tmp = name_db.split(".")
+        tmp = name_dbf.split(".")
         if len(tmp) != 2 or tmp[1] != "db":
             raise NameError("Файл должен иметь разшерение .db")
 
-        self.name_db = name_db
+        self.name_db = name_dbf
         self.connection = sqlite3.connect(self.name_db)
         self.cursor = self.connection.cursor()
         self.header_db: dict = dict()  # Тут храниться типы столбцов таблциы
@@ -47,9 +122,9 @@ class SqlLite:
         # Запись изменений курсора в БД
         self.connection.commit()
 
-    def DeleteTable(self, name_table: str):  # +
+    def DeleteTable(self, name_tables: str):  # +
         # Удалить таблицу если она существует
-        self.cursor.execute("DROP TABLE IF EXISTS {0};".format(name_table))
+        self.cursor.execute("DROP TABLE IF EXISTS {0};".format(name_tables))
         self.header_db = dict()
 
     def DeleteDb(self):  # +
@@ -57,117 +132,74 @@ class SqlLite:
         if exists(self.name_db):
             remove(abspath(self.name_db))
 
-    def CreateTable(self, name_table: str, data: Union[str, Dict]):  # +
-
+    def CreateTable(self, name_tables: str, data: Union[str, Dict]):  # +
         # Конвертация типов в str
         res: str = ""
         if type(data) == str:
             res = data
-            # Конвертировать SQL запросв в header_db
-            for d in res[1:-1:].split(","):
-
-                k, v = d.strip().split(" ")
-                if v in ["TEXT", "text"]:
-                    v = str
-                elif v in ["INTEGER", "integer"]:
-                    v = int
-                elif v in ["REAL", "real"]:
-                    v = float
-                elif v in ["BLOB", "blob"]:
-                    v = bytes
-                elif v in ["NULL", "null"]:
-                    v = None
-                else:
-                    raise TypeError(
-                        "Указан не верный тип данных выбирете TEXT;INTEGER;REAL;BLOB;NULL\n{0}:{1}".format(k, v))
-                self.header_db[k] = v
+            self.header_db = toDictSql(res)
 
         elif type(data) == dict:
-            count_primary_key = False
-            # Конвертация словаря в SQL запрос
-            res += "("
-            for k, v in data.items():
-                res += str(k)
-
-                if type(v) == tuple and len(v) == 2:  # Комбенированная запись с параметрами столбца
-
-                    if v[1] == SqlName.PK or v[1] == SqlName.IDAUTO:  # Проверка уникальности Primary Key в таблицы
-                        if not count_primary_key:
-                            count_primary_key = True
-                        else:
-                            raise LookupError("{0} имеет больше одного primary key".format(name_table))
-
-                    tmp = " {0}".format(v[1])
-                    values = v[0]
-                else:  # Обыная запись столбца
-                    tmp = ''
-                    values = v
-
-                if values == str:
-                    res += " TEXT"
-                elif values == int:
-                    res += " INTEGER"
-                elif values == float:
-                    res += " REAL"
-                elif values == bytes:
-                    res += " BLOB"
-                elif values is None:
-                    res += " NULL"
-                else:
-                    raise TypeError(
-                        "Указан не верный тип данных выбирете str;int;float;None;bytes\n{0}:{1}".format(k, v))
-
-                res += "{0}{1}".format(tmp, ", ")
-
-            res = res[:-2:]
-            res += ");"
+            res = toSqlRequest(data)
             self.header_db = data
 
         # Создание таблицы
-        self.cursor.execute("CREATE TABLE IF NOT EXISTS {0} {1}".format(name_table, res))
+        self.cursor.execute("CREATE TABLE IF NOT EXISTS {0} {1}".format(name_tables, res))
 
-    def ExecuteDb(self, name_table: str, data: Union[List, Tuple, str, bytes]):  # +
+    def ExecuteDb(self, name_tables: str, data: Union[str,
+                                                      List[Union[str, bytes, int, float]],
+                                                      Tuple[Union[str, bytes, int, float]],
+                                                      Dict[str, Union[str, bytes, int, float]]]):  # +
 
         if type(data) == str:
-            if data in "bytes":
+            if data.find("bytes") != -1:
                 raise TypeError("Нельзя отпраять BLOB в формате строки. Воспользуйтесь добавление данных через list")
-            # Запись в курсор
-            self.cursor.execute("INSERT INTO {0} VALUES {1}".format(name_table, data))
-
-        # Конвертация типов в str
-        elif type(data) == tuple or type(data) == list:
-            if len(data) != len(self.header_db):
-                raise IndexError("Разное колличество столбцов таблицы и входных данных")
-
-            res: str = "({}".format("?, " * len(data))[:-2:]
-            res += ");"
-            self.cursor.execute("INSERT INTO {0} VALUES {1}".format(name_table, res), data)
+            self.cursor.execute("INSERT INTO {0} {1} VALUES {2}".format(name_tables, tuple(self.header_db.keys()), data))
 
         else:
-            raise TypeError("Допустимые типы List, Tuple, str")
+            res: str = "({}".format("?, " * len(data))[:-2:]
+            res += ");"
 
-    def GetDb(self, name_table: str) -> list:  # +
+            # Конвертация типа в dict в SQL запрос
+            if type(data) == dict:
+                if tuple(data.keys() - self.header_db.keys()):
+                    raise IndexError("Именя переданного столбца неуществует")
+
+                self.cursor.execute(
+                    "INSERT INTO {0} {1} VALUES {2}".format(name_tables, tuple(data.keys()), res),
+                    tuple(data.values()))
+
+            # Конвертация типов list, tuple в SQL запрос
+            elif type(data) == tuple or type(data) == list:
+                if len(data) != len(self.header_db):
+                    raise IndexError("Разное колличество столбцов таблицы и входных данных")
+
+                self.cursor.execute(
+                    "INSERT INTO {0} {1} VALUES {2}".format(name_tables, tuple(self.header_db.keys()), res),
+                    data)
+
+    def GetDb(self, name_tables: str) -> list:  # +
         """
         Конвертация bytes в обьекта SQl BLOB и обратно
         a = Binary(b"101101")
         print(a.tobytes())
         """
         # Получить данные из таблицы
-        self.cursor.execute('SELECT * FROM {0}'.format(name_table))
-        return self.cursor.fetchall()  # [row for row in self.cursor.execute('SELECT * FROM {0}'.format(name_table))]
+        self.cursor.execute('SELECT * FROM {0}'.format(name_tables))
+        return self.cursor.fetchall()
 
 
 if __name__ == '__main__':
-    pass
-    # name_db = 'example.db'
-    # name_table = "stocks"
-    # sq = SqlLite(name_db)
-    #
-    # sq.DeleteTable(name_table)
-    # sq.CreateTable(name_table, {"name_file":str,"size_file":int})
-    #
-    # for nf in listdir("."):
-    #     if len(nf.split(".")) == 2 and nf.split(".")[1] == "py":
-    #         sq.ExecuteDb(name_table,(nf, getsize(nf)))
-    #
-    # print(sq.GetDb(name_table))
+    name_db = 'example.db'
+    name_table = "stocks"
+    sq = SqlLite(name_db)
+    sq.DeleteTable(name_table)
+
+    sq.CreateTable(name_table, {"id": (int, SqlName.IDAUTO), "name": str, "old": int})
+    print(sq.header_db)
+
+    sq.ExecuteDb(name_table, {"name": "Denis", "old": 21})
+    sq.ExecuteDb(name_table, {"name": "Katy", "old": 23})
+    sq.ExecuteDb(name_table, {"name": "Svetha", "old": 24})
+
+    print(sq.GetDb(name_table))
--- test/test_sqlliteorm.py
+++ test/test_sqlliteorm.py
@@ -20,6 +20,44 @@ class TestSqlLite(unittest.TestCase):
         self.assertRaises(NameError, SqlLite, 'example')
         self.assertRaises(NameError, SqlLite, 'ex.am.pl.et')
 
+    def test_ExecuteDb_dict(self):
+        # Провекра дополнительынх параметоров к созданию таблицы
+        # Провекра записи через dict имен
+        test_header = {"id": (int, SqlName.PK),
+                       "name": str,
+                       "old": (int, SqlName.NND(5)),
+                       "salary": (float, SqlName.NN)}
+        self.sq.CreateTable(self.name_table, test_header)
+        self.assertEqual(self.sq.header_db, test_header)
+        self.sq.ExecuteDb(self.name_table, {"id": 1, "name": "Anton", "old": 30, "salary": 3000.11})
+        self.sq.ExecuteDb(self.name_table, {"id": 2, "name": "Katy", "old": 22, "salary": 3200.23})
+        self.assertEqual(self.sq.GetDb(self.name_table), [(1, 'Anton', 30, 3000.11), (2, 'Katy', 22, 3200.23)])
+        self.sq.DeleteTable(self.name_table)
+
+        # Проверка записи с AUTOINCREMENT
+        test_header = {"id": (int, SqlName.IDAUTO),
+                       "name": str,
+                       "old": (int, SqlName.NND(5)),
+                       "salary": (float, SqlName.NN)}
+        self.sq.CreateTable(self.name_table, test_header)
+        self.assertEqual(self.sq.header_db, test_header)
+        self.sq.ExecuteDb(self.name_table, {"name": "Anton", "old": 30, "salary": 3000.33})
+        self.sq.ExecuteDb(self.name_table, {"name": "Katy", "old": 22, "salary": 3200.54})
+        self.assertEqual(self.sq.GetDb(self.name_table), [(1, 'Anton', 30, 3000.33), (2, 'Katy', 22, 3200.54)])
+        self.sq.DeleteTable(self.name_table)
+
+        # Проверка на неправильное имя столбца в передаче параметров dict
+        test_header = {"id": (int, SqlName.PK),
+                       "name": str,
+                       "old": (int, SqlName.NND(5)),
+                       "salary": (float, SqlName.NN)}
+        self.sq.CreateTable(self.name_table, test_header)
+        self.assertRaises(IndexError, self.sq.ExecuteDb, self.name_table,
+                          {"id": 1, "ERORRRRRRRRR": "Anton", "ol222d": 30, "salary": 3000})
+        self.assertRaises(IndexError, self.sq.ExecuteDb, self.name_table,
+                          {"id": 2, "ERORRR123132RRRRRR": "Katy", "old": 22, "salary": 3200})
+        self.sq.DeleteTable(self.name_table)
+
     def test_CreateTable(self):
         # Првоерка создания таблицы
         test_header = {"date": str, "trans": str, "symbol": str, "qty": float, "price": float}
@@ -28,35 +66,7 @@ class TestSqlLite(unittest.TestCase):
         self.sq.DeleteTable(self.name_table)
 
         # Если изменить test_header то нужно переписать test_header на sql запрос
-        # (date text, trans text, symbol text, qty real, price real)
         self.sq.CreateTable(self.name_table, "(date text, trans text, symbol text, qty real, price real)")
-        self.assertEqual(self.sq.header_db, test_header)
-        self.sq.DeleteTable(self.name_table)
-
-        # Провекра Двойного создания Primary Key
-        test_header = {"id": (int, SqlName.IDAUTO),
-                       "url": (int, SqlName.PK),
-                       "name": str,
-                       "old": (int, SqlName.NND(5)),
-                       "salary": (float, SqlName.NN)}
-        self.assertRaises(LookupError, self.sq.CreateTable, (self.name_table, test_header))
-
-
-        # Провекра дополнительынх параметоров к созданию таблицы
-        test_header = {"id": (int, SqlName.IDAUTO),
-                       "name": str,
-                       "old": (int, SqlName.NND(5)),
-                       "salary": (float, SqlName.NN)}
-        self.sq.CreateTable(self.name_table, test_header)
-        self.assertEqual(self.sq.header_db, test_header)
-        self.sq.DeleteTable(self.name_table)
-
-
-        test_header = {"id": (int, SqlName.PK),
-                       "name": str,
-                       "old": (int, SqlName.NND(5)),
-                       "salary": (float, SqlName.NN)}
-        self.sq.CreateTable(self.name_table, test_header)
         self.assertEqual(self.sq.header_db, test_header)
         self.sq.DeleteTable(self.name_table)
 
@@ -67,21 +77,30 @@ class TestSqlLite(unittest.TestCase):
         # Если изменить test_header то нужно переписать test_header на sql запрос
         self.assertRaises(TypeError, self.sq.CreateTable, self.name_table,
                           "(date text, trans text, symbol int, qty real, price real)")
+        # Провекра Двойного создания Primary Key
+        test_header = {"id": (int, SqlName.IDAUTO),
+                       "url": (int, SqlName.PK),
+                       "name": str,
+                       "old": (int, SqlName.NND(5)),
+                       "salary": (float, SqlName.NN)}
+        self.assertRaises(LookupError, self.sq.CreateTable, self.name_table, test_header)
 
     def test_ExecuteDb_and_GetDb(self):
         # Првоерка коректности записи данных в таблицу
+        # Через dict
         test_header = {"date": str, "trans": str, "symbol": str, "qty": float, "price": float}
         test_data = ('2006-01-05', 'BUY', 'RAT', 100, 35.14)
         self.sq.CreateTable(self.name_table, test_header)
         self.sq.ExecuteDb(self.name_table, test_data)
         self.assertEqual(self.sq.GetDb(self.name_table)[0], test_data)
         self.sq.DeleteTable(self.name_table)
-
+        # Через SQL запрос
         self.sq.CreateTable(self.name_table, test_header)
         self.sq.ExecuteDb(self.name_table, "('2006-01-05','BUY','RAT',100,35.14)")
         self.assertEqual(self.sq.GetDb(self.name_table)[0], test_data)
         self.sq.DeleteTable(self.name_table)
 
+    def test_ExecuteDb_Blob(self):
         # Проверка записи BLOB через
         # Tuple
         test_header = {"str": str, "int": int, "float": float, "bytes": bytes}
@@ -97,6 +116,7 @@ class TestSqlLite(unittest.TestCase):
         self.sq.ExecuteDb(self.name_table, list(test_data))
         self.assertEqual(self.sq.GetDb(self.name_table)[0], test_data)
         self.sq.DeleteTable(self.name_table)
+
         # Проверка попытки записи типа BLOB через строку -> должны быть ошибка TypeError
         test_header = {"str": str, "int": int, "float": float, "bytes": bytes}
         test_data = "('text', '123', '122.32', '{0}')".format(b"0101")
@@ -111,11 +131,16 @@ class TestSqlLite(unittest.TestCase):
         self.sq.CreateTable(self.name_table, test_header)
 
         self.assertRaises(IndexError, self.sq.ExecuteDb, self.name_table,
-                          ('2006-01-05', 'BUY', 'RAT', 100, 35.14, 100010001))
+                          ('2006-01-05', 'BUY', 'RAT', 100, 35.14, 100010001))  # Привышение длины tuple,list
+
+        self.assertRaises(IndexError, self.sq.ExecuteDb, self.name_table,
+                          ('2006-01-05', 'BUY', 35.14, 100010001))  # Маленькая длины tuple,list
+
         self.assertRaises(OperationalError, self.sq.ExecuteDb, self.name_table,
-                          "('2006-01-05','Вредное слово','BUY','RAT',100,35.14)")
-        self.assertRaises(TypeError, self.sq.ExecuteDb, self.name_table,
-                          {'2006-01-05', 'BUY', 'RAT', 100, 35.14, 100010001})
+                          "('2006-01-05','Вредное слово','BUY','RAT',100,35.14)")  # Привышение длины для str
+
+        self.assertRaises(OperationalError, self.sq.ExecuteDb, self.name_table,
+                          "('2006-01-05','Вредное слово',100,35.14)")  # Маленькая длины для str
 
     def test_DeleteDb(self):
         # Провекра удаления Бд
